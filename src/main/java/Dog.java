5721cc9ed7ecd83ac4ca48a940a7d783f950bd44
athenian <bivboh@gmail.com>
2021-11-01 00:47:21 -0700

    done

--- src/main/java/Dog.java
+++ src/main/java/Dog.java
@@ -1,18 +1,51 @@
 public class Dog extends Pet {
-    // Instance variable(s)
 
+    // Instance variable(s)
+    private String breed;
 
     // Constructors
+    public Dog(String a, int b, String c){
+        super(a, b);
+        breed = c;
+    }
+    public Dog(){
+        super("Max", 1);
+        breed = "golden retriever";
+    }
     
 
     // makeNoise() method
+    public void makeNoise() {
+        System.out.println("Woof!");
+    }
 
 
     // toString method
+    public String toString() {
+        String str = "Name: " + getName() + ", Age: " + getAge() + "Breed: " + breed;
+        return str;
+    }
 
 
     // Getter
+    public String getName() {
+        return getName();
+    }
 
+    public int getAge() {
+        return getAge();
+    }
+
+    public String getBreed() {return this.breed;}
 
     // Setter
+
+
+    public void setBreed(String breed) {
+        // Don't allow blank names
+        if (breed.trim().length() != 0) {
+            this.breed = breed;
+        }
+    }
+
 }
--- src/main/java/DogClient.java
+++ src/main/java/DogClient.java
@@ -1,5 +1,30 @@
 public class DogClient {
     public static void main(String[] args) {
-        
+        Dog toffee = new Dog("Toffee", 1, "goldendoodle");
+        Dog doggie = new Dog();
+        toffee.makeNoise();
+        doggie.makeNoise();
+        toffee.toString();
+        doggie.toString();
+        toffee.getName();
+        toffee.getAge();
+        toffee.getBreed();
+        doggie.getName();
+        doggie.getAge();
+        doggie.getBreed();
+        toffee.setName("Toffee M");
+        toffee.setAge(2);
+        toffee.setBreed("medium goldendoodle");
+        doggie.setName("bill");
+        doggie.setAge(4);
+        doggie.setBreed("german shepherd");
+        toffee.toString();
+        doggie.toString();
+        toffee.getName();
+        toffee.getAge();
+        toffee.getBreed();
+        doggie.getName();
+        doggie.getAge();
+        doggie.getBreed();
     }
 }
